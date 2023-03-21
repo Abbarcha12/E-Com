1e96693523f37424d4ff26a606e694beaf8176a7
Barcha1 <abbarcha123456@gmail.com>
2023-03-21 15:25:29 +0500

    latest changes

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Container} from "react-bootstrap"
+import HomeScreen from './screens/HomeScreen';
+import Productscreen from './screens/Productscreen';
+import { Route,Routes } from 'react-router-dom';
 function App() {
   return (
    <>
-   Hello
+   <Header/>
+
+  <main className='py-3'>
+ <Container >
+
+<Routes>
+  <Route path="/" element={<HomeScreen/>} exact />
+  <Route path="/product/:id" element={<Productscreen/>} exact />
+
+</Routes>
+ </Container>
+  </main>
+   <Footer/>
    </>
   );
 }
